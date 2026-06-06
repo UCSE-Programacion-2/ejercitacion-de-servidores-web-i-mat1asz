abe525068c3089ecab4e52e18d0cdc209c164871
Matias <matias2004camposxd@gmail.com>
2026-06-06 02:04:43 -0300

    feat: implementar servidor HTTP con rutas y manejo de 404 Closes #1

--- src/server.js
+++ src/server.js
@@ -10,6 +10,24 @@ const http = require('http');
 function createServer() {
   const server = http.createServer((req, res) => {
     /* Aquí la lógica del enrutador */
+    const encabezados = { 'Content-Type': 'text/plain; charset=utf-8' };
+
+    if (req.url === '/') {
+      res.writeHead(200, encabezados);
+      res.end('Bienvenid@s! Gracias por tu visita.');
+    } else if (req.url === '/nosotros') {
+      res.writeHead(200, encabezados);
+      res.end('Bienvenid@s a saber + de nosotros :)');
+    } else if (req.url === '/cursos') {
+      res.writeHead(200, encabezados);
+      res.end('Te interesan nuestros cursos. Vení a conocer la oferta!');
+    } else if (req.url === '/contacto') {
+      res.writeHead(200, encabezados);
+      res.end('Si querés contactarnos, hacelo a este Email: :)');
+    } else {
+      res.writeHead(404, encabezados);
+      res.end('No se ha encontrado la ruta ingresada.');
+    }
   });
 
   return server;
